1bd1f5aca3c7039606f33098da06cde877841a18
Henley Zhang <henleyzhang0925@gmail.com>
2025-09-15 15:55:15 -0700

    fix bug

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ let backgroundimg = document.getElementById("newhome");
 
 //
 // artbackground.style.backgroundImage = "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("+randomImage+")";
-let images = ["/arts_compressed/1darkpainting.png", "/arts_compressed/bridge.jpg", "/arts_compressed/cliff.jpg", "/arts_compressed/roof.jpg",   "/arts_compressed/tower.jpeg",  "/arts_compressed/010.jpg", "/arts_compressed/RedCottage.jpg"]
-//let randomNum = Math.floor(Math.random() * images.length);
-let randomNum = 0
+let images = ["/arts_compressed/1darkpainting.jpg", "/arts_compressed/bridge.jpg", "/arts_compressed/cliff.jpg", "/arts_compressed/roof.jpg",   "/arts_compressed/tower.jpg",  "/arts_compressed/010.jpg", "/arts_compressed/RedCottage.jpg"]
+let randomNum = Math.floor(Math.random() * images.length);
+//let randomNum = 0
 let randomImage = images[randomNum];
 function scrollfunction(e){
     let currentScroll = window.pageYOffset;
